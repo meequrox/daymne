a52e063730cb45e10b7cbfbffcc2d5a65d1cfc50
meequrox <117393211+meequrox@users.noreply.github.com>
2024-05-21 03:44:41 +0700

    Implement update check command

--- src/main.v
+++ src/main.v
@@ -3,25 +3,73 @@ module main
 import os
 import json
 import cli
+import net.http
+import time
 
-struct Extension {
+struct LocalExtensionMetadata {
+	installed_timestamp i64    @[json: 'installedTimestamp'; required]
+	source              string @[required]
+}
+
+struct LocalExtensionLocation {
+	mid    int    @[json: '\$mid'; required]
+	path   string @[required]
+	scheme string @[required]
+}
+
+struct LocalExtensionIdentifier {
+	id string @[required]
+}
+
+struct LocalExtension {
 	version           string @[required]
 	relative_location string @[json: 'relativeLocation'; required]
 
-	identifier struct {
-		id string @[required]
-	} @[required]
+	identifier LocalExtensionIdentifier @[required]
 
-	location struct {
-		mid    int    @[json: '\$mid'; required]
-		path   string @[required]
-		scheme string @[required]
-	} @[required]
+	location LocalExtensionLocation @[required]
 
-	metadata struct {
-		installed_timestamp i64    @[json: 'installedTimestamp'; required]
-		source              string @[required]
-	} @[required]
+	metadata LocalExtensionMetadata @[required]
+}
+
+struct RemoteAsset {
+	asset_type string @[json: 'assetType']
+	source     string
+}
+
+struct RemoteVersion {
+	version         string
+	target_platform string        @[json: 'targetPlatform']
+	files           []RemoteAsset
+}
+
+struct RemoteResultMetadataItems {
+	count int
+	name  string
+}
+
+struct RemoteResultMetadata {
+	metadata_items []RemoteResultMetadataItems @[json: 'metadataItems']
+	metadata_type  string                      @[json: 'metadataType']
+}
+
+struct RemoteResultExtension {
+	versions []RemoteVersion
+}
+
+struct RemoteResult {
+	extensions      []RemoteResultExtension
+	result_metadata []RemoteResultMetadata  @[json: 'resultMetadata']
+}
+
+struct RemoteExtensions {
+	results []RemoteResult
+}
+
+struct RemoteExtension {
+mut:
+	version     string
+	package_url string
 }
 
 fn get_root_path() string {
@@ -32,15 +80,15 @@ fn get_root_config_path() string {
 	return os.join_path_single(get_root_path(), 'extensions.json')
 }
 
-fn get_extensions_list() []Extension {
+fn get_local_extensions_list() []LocalExtension {
 	config_str := os.read_file(get_root_config_path()) or { panic(err) }
-	list := json.decode([]Extension, config_str) or { panic(err) }
+	list := json.decode([]LocalExtension, config_str) or { panic(err) }
 
 	return list.sorted(a.identifier.id < b.identifier.id)
 }
 
-fn print_extensions() {
-	for extension in get_extensions_list() {
+fn print_local_extensions() {
+	for extension in get_local_extensions_list() {
 		println('${extension.identifier.id} ${extension.version}')
 	}
 }
@@ -48,7 +96,90 @@ fn print_extensions() {
 fn print_info() {
 	println('Root path: ${get_root_path()}')
 	println('Config path: ${get_root_config_path()}')
-	println('Extensions count: ${get_extensions_list().len}')
+	println('Extensions count: ${get_local_extensions_list().len}')
+}
+
+enum RemoteQueryFlag {
+	include_versions             = 0x1
+	include_files                = 0x2
+	include_category_and_tags    = 0x4
+	include_shared_accounts      = 0x8
+	include_version_properties   = 0x10
+	exclude_non_validated        = 0x20
+	include_installation_targets = 0x40
+	include_asset_uri            = 0x80
+	include_statistics           = 0x100
+	include_latest_version_only  = 0x200
+	unpublished                  = 0x1000
+	include_name_conflict_info   = 0x8000
+}
+
+fn find_package_url(assets []RemoteAsset) string {
+	for asset in assets {
+		if asset.asset_type == 'Microsoft.VisualStudio.Services.VSIXPackage' {
+			return asset.source
+		}
+	}
+
+	return ''
+}
+
+fn match_remote_extension(exts RemoteExtensions) RemoteExtension {
+	if exts.results.len > 0 && exts.results[0].result_metadata[0].metadata_items[0].count > 0 {
+		versions := exts.results[0].extensions[0].versions
+
+		mut ext := RemoteExtension{
+			version: ''
+			package_url: ''
+		}
+
+		platform := 'linux-x64' // TODO: get current platform in runtime
+
+		for version in versions {
+			if version.target_platform == '' || version.target_platform == platform {
+				ext.version = version.version
+				ext.package_url = find_package_url(version.files)
+				break
+			}
+		}
+
+		return ext
+	}
+
+	return RemoteExtension{}
+}
+
+fn request_extension_from_remote(id string) RemoteExtension {
+	query_flags := int(RemoteQueryFlag.include_files) | int(RemoteQueryFlag.include_installation_targets) | int(RemoteQueryFlag.include_latest_version_only)
+	filter_type := 7
+
+	url := 'https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery'
+	body := '{"filters":[{"criteria":[{"filterType":${filter_type},"value":"${id}"}]}],"flags":${query_flags}}'
+
+	resp := http.post_json(url, body) or { panic(err) }
+	time.sleep(100_000_000) // 100 ms
+
+	if resp.status() == http.Status.ok {
+		exts := json.decode(RemoteExtensions, resp.body) or { panic(err) }
+		return match_remote_extension(exts)
+	} else {
+		// TODO: remove in release
+		println('Request extension from remote: code ${resp.status_code}; body ${resp.body}')
+	}
+
+	return RemoteExtension{}
+}
+
+fn check_extensions_updates() {
+	for local_ext in get_local_extensions_list() {
+		remote_ext := request_extension_from_remote(local_ext.identifier.id)
+
+		if local_ext.version != remote_ext.version {
+			println('${local_ext.identifier.id} ${local_ext.version} -> ${remote_ext.version}')
+		}
+
+		break
+	}
 }
 
 fn main() {
@@ -68,7 +199,15 @@ fn main() {
 	list_cmd := cli.Command{
 		name: 'list'
 		execute: fn (cmd cli.Command) ! {
-			print_extensions()
+			print_local_extensions()
+			return
+		}
+	}
+
+	check_cmd := cli.Command{
+		name: 'check'
+		execute: fn (cmd cli.Command) ! {
+			check_extensions_updates()
 			return
 		}
 	}
@@ -82,6 +221,7 @@ fn main() {
 		commands: [
 			info_cmd,
 			list_cmd,
+			check_cmd,
 		]
 		defaults: struct {
 			help: cli.CommandFlag{
@@ -93,6 +233,7 @@ fn main() {
 			}
 		}
 	}
+
 	app.setup()
 	app.parse(os.args)
 }
