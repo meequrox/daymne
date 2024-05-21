a86b101bfcba8e641fb9e732c3ed9904acb3fc7e
meequrox <117393211+meequrox@users.noreply.github.com>
2024-05-22 03:47:26 +0700

    Complete check command

--- src/main.v
+++ src/main.v
@@ -5,72 +5,9 @@ import json
 import cli
 import net.http
 import time
-
-struct LocalExtensionMetadata {
-	installed_timestamp i64    @[json: 'installedTimestamp'; required]
-	source              string @[required]
-}
-
-struct LocalExtensionLocation {
-	mid    int    @[json: '\$mid'; required]
-	path   string @[required]
-	scheme string @[required]
-}
-
-struct LocalExtensionIdentifier {
-	id string @[required]
-}
-
-struct LocalExtension {
-	version           string @[required]
-	relative_location string @[json: 'relativeLocation'; required]
-
-	identifier LocalExtensionIdentifier @[required]
-
-	location LocalExtensionLocation @[required]
-
-	metadata LocalExtensionMetadata @[required]
-}
-
-struct RemoteAsset {
-	asset_type string @[json: 'assetType']
-	source     string
-}
-
-struct RemoteVersion {
-	version         string
-	target_platform string        @[json: 'targetPlatform']
-	files           []RemoteAsset
-}
-
-struct RemoteResultMetadataItems {
-	count int
-	name  string
-}
-
-struct RemoteResultMetadata {
-	metadata_items []RemoteResultMetadataItems @[json: 'metadataItems']
-	metadata_type  string                      @[json: 'metadataType']
-}
-
-struct RemoteResultExtension {
-	versions []RemoteVersion
-}
-
-struct RemoteResult {
-	extensions      []RemoteResultExtension
-	result_metadata []RemoteResultMetadata  @[json: 'resultMetadata']
-}
-
-struct RemoteExtensions {
-	results []RemoteResult
-}
-
-struct RemoteExtension {
-mut:
-	version     string
-	package_url string
-}
+import v.pref
+import extension
+import semver
 
 fn get_root_path() string {
 	return os.join_path(os.home_dir(), '.vscode-oss', 'extensions')
@@ -80,9 +17,9 @@ fn get_root_config_path() string {
 	return os.join_path_single(get_root_path(), 'extensions.json')
 }
 
-fn get_local_extensions_list() []LocalExtension {
+fn get_local_extensions_list() []extension.LocalExtension {
 	config_str := os.read_file(get_root_config_path()) or { panic(err) }
-	list := json.decode([]LocalExtension, config_str) or { panic(err) }
+	list := json.decode([]extension.LocalExtension, config_str) or { panic(err) }
 
 	return list.sorted(a.identifier.id < b.identifier.id)
 }
@@ -99,22 +36,7 @@ fn print_info() {
 	println('Extensions count: ${get_local_extensions_list().len}')
 }
 
-enum RemoteQueryFlag {
-	include_versions             = 0x1
-	include_files                = 0x2
-	include_category_and_tags    = 0x4
-	include_shared_accounts      = 0x8
-	include_version_properties   = 0x10
-	exclude_non_validated        = 0x20
-	include_installation_targets = 0x40
-	include_asset_uri            = 0x80
-	include_statistics           = 0x100
-	include_latest_version_only  = 0x200
-	unpublished                  = 0x1000
-	include_name_conflict_info   = 0x8000
-}
-
-fn find_package_url(assets []RemoteAsset) string {
+fn find_package_url(assets []extension.RemoteAsset) string {
 	for asset in assets {
 		if asset.asset_type == 'Microsoft.VisualStudio.Services.VSIXPackage' {
 			return asset.source
@@ -124,16 +46,39 @@ fn find_package_url(assets []RemoteAsset) string {
 	return ''
 }
 
-fn match_remote_extension(exts RemoteExtensions) RemoteExtension {
+fn get_current_platform() string {
+	host_os := pref.get_host_os().str()
+	host_arch := pref.get_host_arch().str()
+
+	mut platform := 'unknown'
+	mut arch := 'unknown'
+
+	match host_os {
+		'Windows' { platform = 'win32' }
+		'Linux' { platform = 'linux' }
+		'MacOS' { platform = 'darwin' }
+		else { platform = host_os.to_lower() }
+	}
+
+	match host_arch {
+		'i386' { arch = 'ia32' }
+		'amd64' { arch = 'x64' }
+		else { arch = host_arch }
+	}
+
+	return '${platform}-${arch}'
+}
+
+fn match_remote_extension(exts extension.RemoteExtensions) extension.RemoteExtension {
 	if exts.results.len > 0 && exts.results[0].result_metadata[0].metadata_items[0].count > 0 {
 		versions := exts.results[0].extensions[0].versions
 
-		mut ext := RemoteExtension{
+		mut ext := extension.RemoteExtension{
 			version: ''
 			package_url: ''
 		}
 
-		platform := 'linux-x64' // TODO: get current platform in runtime
+		platform := get_current_platform()
 
 		for version in versions {
 			if version.target_platform == '' || version.target_platform == platform {
@@ -146,41 +91,57 @@ fn match_remote_extension(exts RemoteExtensions) RemoteExtension {
 		return ext
 	}
 
-	return RemoteExtension{}
+	return extension.RemoteExtension{}
 }
 
-fn request_extension_from_remote(id string) RemoteExtension {
-	query_flags := int(RemoteQueryFlag.include_files) | int(RemoteQueryFlag.include_installation_targets) | int(RemoteQueryFlag.include_latest_version_only)
-	filter_type := 7
+fn request_extension_from_remote(id string) extension.RemoteExtension {
+	query_flags := int(extension.RemoteQueryFlag.include_files) | int(extension.RemoteQueryFlag.include_installation_targets) | int(extension.RemoteQueryFlag.include_latest_version_only)
 
 	url := 'https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery'
-	body := '{"filters":[{"criteria":[{"filterType":${filter_type},"value":"${id}"}]}],"flags":${query_flags}}'
+	body := '{"filters":[{"criteria":[{"filterType":7,"value":"${id}"}]}],"flags":${query_flags}}'
+	ua := 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) UpNote/9.3.0 Chrome/122.0.6261.156 Electron/29.3.1 Safari/537.36'
 
-	resp := http.post_json(url, body) or { panic(err) }
-	time.sleep(100_000_000) // 100 ms
+	mut req := http.new_request(http.Method.post, url, body)
+	req.add_header(http.CommonHeader.content_type, 'application/json')
+	req.add_header(http.CommonHeader.accept, 'application/json;api-version=3.0-preview.1')
+	req.add_header(http.CommonHeader.user_agent, ua)
+
+	req.read_timeout = 5 * time.second
+	req.write_timeout = req.read_timeout
+
+	println('${time.now()}: do req for ${id}')
+	resp := req.do() or { panic(err) }
+	time.sleep(100 * time.millisecond)
+	println('${time.now()}: end req for ${id}')
 
 	if resp.status() == http.Status.ok {
-		exts := json.decode(RemoteExtensions, resp.body) or { panic(err) }
+		exts := json.decode(extension.RemoteExtensions, resp.body) or { panic(err) }
 		return match_remote_extension(exts)
 	} else {
 		// TODO: remove in release
 		println('Request extension from remote: code ${resp.status_code}; body ${resp.body}')
 	}
 
-	return RemoteExtension{}
+	return extension.RemoteExtension{}
 }
 
 fn check_extensions_updates() {
 	for local_ext in get_local_extensions_list() {
 		remote_ext := request_extension_from_remote(local_ext.identifier.id)
 
-		if local_ext.version != remote_ext.version {
+		local_version := semver.from(local_ext.version) or { panic(err) }
+		remote_version := semver.from(remote_ext.version) or { panic(err) }
+
+		if remote_version > local_version {
 			println('${local_ext.identifier.id} ${local_ext.version} -> ${remote_ext.version}')
 		}
-
-		break
 	}
 }
+
+// TODO: Move functions to packages
+// TODO: Install command
+// TODO: Upgrade (all) command
+// ? Pipe upgrade through install?
 
 fn main() {
 	default_cmd := fn (cmd cli.Command) ! {
